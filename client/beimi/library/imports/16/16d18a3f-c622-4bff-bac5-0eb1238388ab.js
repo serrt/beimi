efb0dab906ba69bf4ef6d59654bd904d20a5d61d
贝密游戏 <open@ukewo.cn>
2017-08-04 21:53:42 +0800

    no commit message

--- client/beimi/library/imports/16/16d18a3f-c622-4bff-bac5-0eb1238388ab.js
+++ client/beimi/library/imports/16/16d18a3f-c622-4bff-bac5-0eb1238388ab.js
@@ -23,9 +23,21 @@ cc.Class({
     onLoad: function onLoad() {
         var self = this;
         this._dialog = cc.find("Canvas/dialog");
+        this._button = cc.find("Canvas/button");
+        this._button.active = false;
+        this._girl = cc.find("Canvas/splash/background/girl");
+        this._animCtrl = this._girl.getComponent(cc.Animation);
     },
     onClick: function onClick() {
         this._dialog.active = false;
+        this._button.active = true;
+        this._animCtrl.play("girl_to_left");
+    },
+    onShowClick: function onShowClick() {
+        this._button.active = false;
+        this._dialog.active = true;
+        var anim = this.getComponent(cc.Animation);
+        this._animCtrl.play("girl_to_right");
     }
 
     // called every frame, uncomment this function to activate update callback
--- client/beimi/temp/BackupAssets/assets/script/action/login/dialog.js
+++ client/beimi/temp/BackupAssets/assets/script/action/login/dialog.js
@@ -17,9 +17,21 @@ cc.Class({
     onLoad: function () {
         var self = this;
         this._dialog = cc.find("Canvas/dialog");
+        this._button = cc.find("Canvas/button");
+        this._button.active = false ;
+        this._girl = cc.find("Canvas/splash/background/girl");
+        this._animCtrl = this._girl.getComponent(cc.Animation);
     },
     onClick:function(){
         this._dialog.active = false ;
+        this._button.active = true ;
+        this._animCtrl.play("girl_to_left");
+    },
+    onShowClick:function(){
+        this._button.active = false ;
+        this._dialog.active = true ;
+        var anim = this.getComponent(cc.Animation);
+        this._animCtrl.play("girl_to_right");
     }
 
     // called every frame, uncomment this function to activate update callback
